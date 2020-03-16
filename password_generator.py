33c5ccfaf5769908c0c584dd3a4d0a30ba6707ac
justinas2314 <53621494+justinas2314@users.noreply.github.com>
2020-03-16 14:15:54 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -3,20 +3,19 @@ import random
 import clipboard
 
 nums, upper, lower, chrs = "0123456789", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", r"!@#$%^&*()\")"
-
 all_chars = nums + upper + lower + chrs
 
 
-def main():
+def main():  # this is used to make sure that the password fits the requirements for most websites 
 	while True:
 		string = ''
-		string = string.join(random.choices(all_chars, k=64))
+		string = string.join(random.choices(all_chars, k=64))  # k is the length of the password
 		print(string)
 		b_nums = False
 		b_upper = False
 		b_lower = False
 		b_chrs = False
-		for i in string:
+		for i in string:  # makes sure there is atleast one of each type of char in the password
 			if i in nums:
 				b_nums = True
 			elif i in upper:
