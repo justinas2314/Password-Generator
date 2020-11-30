16fc5601c7ba643f566990c70aaf3998fb659f4a
Justinas Staskevicius <staskevicius14@gmail.com>
2020-11-30 18:03:39 +0200

    looks better

--- password_generator.py
+++ password_generator.py
@@ -5,26 +5,15 @@ import pyperclip
 def main(length: int):
     nums, upper, lower, chrs = "0123456789", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", \
                                "abcdefghijklmnopqrstuvwxyz", r" !@#$%^&*()\")[]{}"
-    all_chars = nums + upper + lower + chrs
     while True:
-        string = ''.join(random.choices(all_chars, k=length))
-        print(string)
-        b_nums = False
-        b_upper = False
-        b_lower = False
-        b_chrs = False
-        for i in string:
-            if i in nums:
-                b_nums = True
-            elif i in upper:
-                b_upper = True
-            elif i in lower:
-                b_lower = True
-            elif i in chrs:
-                b_chrs = True
-            if all((b_nums, b_upper, b_lower, b_chrs)):
-                pyperclip.copy(string)
-                return None
+        string = ''.join(random.choices(nums + upper + lower + chrs, k=length))
+        string_as_set = set(string)
+        if all((set(nums) & string_as_set != {},
+                set(upper) & string_as_set != {},
+                set(lower) & string_as_set != {},
+                set(chrs) & string_as_set != {})):
+            pyperclip.copy(string)
+            return
 
 
 if __name__ == "__main__":
