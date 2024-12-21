2dc38b5d8737b6e6d5c38a9f631940f6ec34fdae
Justinas Staskevičius <justinas.staskevicius@tuta.io>
2024-12-21 20:39:02 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,4 +1,4 @@
-import random
+import secrets
 import pyperclip
 
 
@@ -6,12 +6,13 @@ def main(length: int):
     nums, upper, lower, chrs = "0123456789", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", \
                                "abcdefghijklmnopqrstuvwxyz", r" !@#$%^&*()\")[]{}"
     while True:
-        string = ''.join(random.choices(nums + upper + lower + chrs, k=length))
+        string = ''.join(secrets.choice(nums + upper + lower + chrs) for _ in range(length))
         string_as_set = set(string)
         if all((len(set(nums) & string_as_set),
                 len(set(upper) & string_as_set),
                 len(set(lower) & string_as_set),
                 len(set(chrs) & string_as_set))):
+            print(string)
             return string
 
 
